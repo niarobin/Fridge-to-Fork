6bb3e106037bcbd062a22a560df22e693634cce9
Hyunjin Lee <hyunjin.lee2@duke.edu>
2024-07-08 11:31:33 -0400

    attempted to make MyRecipes page and button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 
+// import pages and buttons
+import { 
+  BrowserRouter as Router, Routes, 
+  Route 
+} from "react-router-dom"; 
+import MyRecipes from "./pages/MyRecipes"; 
+import MyRecipesButton from "./components/MyRecipesButton"; 
+
 //<text1 id="1" x="100" y="200" font-family="Yeseva One" font-size="54" fill="white">Fridge to Fork.</text1>
 
 <color name="lightapple">#E8D7D7</color> //for keeping track of colors??
@@ -21,11 +29,20 @@ function App() {
         >
         </a>
       </header>
+
+      {/* route buttons */}
+      <Router> 
+        <MyRecipesButton to="my-recipes" /> 
+        <Routes> 
+            <Route path="/my-recipes"
+                element={<MyRecipes />} /> 
+        </Routes> 
+      </Router> 
     </div>
   );
 }
 
-function MyRecipesButton() {
+function My_RecipesButton() {
   return (
     <button>My Recipes</button>
   );
